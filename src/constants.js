106b96606beee4e5e0a7fbf4f6662399f48cb234
Mark Katerberg <katerberg@fastmail.fm>
2020-02-29 12:14:14 -0600

    vim mappings

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,11 @@ export const colors = {
 
 export const validKeyMap = {
   38: 0, // Up
+  75: 0, // Up
   39: 1, // Right
+  76: 1, // Right
   40: 2, // Down
+  74: 2, // Down
   37: 3, // Left
+  72: 3, // Left
 };
