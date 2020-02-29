ee06722c0327d8f7a8c76bf09da7e9e2936f79af
Mark Katerberg <katerberg@fastmail.fm>
2020-02-28 23:12:43 -0600

    Log the directions

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,10 @@
 export const colors = {
   YELLOW: '#ff0',
 };
+
+export const validKeyMap = {
+  37: 'LEFT',
+  38: 'UP',
+  39: 'RIGHT',
+  40: 'DOWN',
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import "regenerator-runtime/runtime";
+import 'regenerator-runtime/runtime';
 import {Display, Map, RNG, Scheduler} from 'rot-js';
-import {colors} from './constants';
+import {colors, validKeyMap} from './constants';
 
 class Player {
   constructor(x, y) {
     this.x = x;
     this.y = y;
     this.draw();
+    this.resolver = () => {}; // eslint-disable-line no-empty-function
+  }
+
+  handleEvent({keyCode}) {
+    if (!(keyCode in validKeyMap)) {
+      return;
+    }
+    console.log(validKeyMap[keyCode]);
+    window.removeEventListener('keydown', this);
+    this.resolver();
   }
 
   act() {
-    return new Promise(resolve => setTimeout(resolve, 500)); // pause
+    return new Promise(resolve => {
+      window.addEventListener('keydown', this);
+      this.resolver = resolve;
+    });
   }
 
   draw() {
